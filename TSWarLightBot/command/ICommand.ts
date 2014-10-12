fefa1ca80f8d44bff20c41db2fd2816653d7828e
taeke <taekevdv@gmail.com>
2014-10-12 21:14:57 +0200

    Added ICommands

---
+++ TSWarLightBot/command/ICommand.ts
@@ -0,0 +1,19 @@
+﻿/**
+ * Warlight AI Game Bot
+ *
+ * Oktober 2014
+ *
+ * Based on Niko van Meurs javascript bot from http://theaigames.com/competitions/warlight-ai-challenge/getting-started
+ *
+ * @authors Marcel van de Kamp and Taeke van der Veen
+ * @License MIT License (http://opensource.org/Licenses/MIT)
+ */
+
+import ICommandResult = require('ICommandResult');
+import ICommandData = require('ICommandData');
+
+interface ICommand {
+    getCommandResult(data: ICommandData): ICommandResult;
+}
+
+export = ICommand;
