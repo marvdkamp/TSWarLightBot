04eb4c49de9fccce16fe5d837894109606b31c28
taeke <taekevdv@gmail.com>
2014-10-13 08:55:11 +0200

    Corrected ignored ts file

---
+++ TSWarLightBot/command/Pick_starting_regions.ts
@@ -0,0 +1,22 @@
+﻿/**
+ * Warlight AI Game Bot
+ *
+ * Oktober 2014
+ *
+ * Based on Niko van Meurs javascript bot from http://theaigames.com/competitions/warlight-ai-challenge/getting-started
+ *
+ * @authors Marcel van de Kamp and Taeke van der Veen
+ * @License MIT License (http://opensource.org/Licenses/MIT)
+ */
+
+import ICommand = require('ICommand');
+import ICommandResult = require('ICommandResult');
+import ICommandData = require('ICommandData');
+
+class Pick_starting_regions implements ICommand {
+    public getCommandResult(data: ICommandData): ICommandResult {
+        return null;
+    }
+}
+
+export = Pick_starting_regions;
