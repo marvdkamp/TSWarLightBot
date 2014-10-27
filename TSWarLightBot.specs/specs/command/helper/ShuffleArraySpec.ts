c3225da9fd95e1c862a4812e1cf82813b46b0b3c
taeke <taekevdv@gmail.com>
2014-10-27 16:00:38 +0100

    Added spec file for ShuffleArray

---
+++ TSWarLightBot.specs/specs/command/helper/ShuffleArraySpec.ts
@@ -0,0 +1,23 @@
+﻿/*
+ * Warlight AI Game Bot
+ *
+ * Oktober 2014
+ *
+ * Based on Niko van Meurs javascript bot from http://theaigames.com/competitions/warlight-ai-challenge/getting-started
+ *
+ * @authors Marcel van de Kamp and Taeke van der Veen
+ * @License MIT License (http://opensource.org/Licenses/MIT)
+ */
+
+/// <reference path="../../../Scripts/typings/jasmine/legacy/jasmine-1.3.d.ts" />
+
+'use strict';
+
+describe('shuffleArray', (): void => {
+    // Class for unit under test and variable for instance of unit under test.
+    var ShuffleArray: any = require('../../../../TSWarLightBot/command/helper/ShuffleArray');
+    var shuffleArray: any;
+
+    describe('shuffle', (): void => {
+    });
+});
